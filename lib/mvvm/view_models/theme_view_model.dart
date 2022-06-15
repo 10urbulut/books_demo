2e33941bddf2de56f60fecc85c525fbf00f9d733
Onur Bulut <10urbulut@gmail.com>
2022-06-15 21:53:06 +0300

    Add theme view model

---
+++ lib/mvvm/view_models/theme_view_model.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ThemeViewModel extends ChangeNotifier {
+  ThemeMode _themeMode = ThemeMode.system;
+  ThemeMode get themeMode => _themeMode;
+
+  set setThemeMode(ThemeMode value) {
+    _themeMode = value;
+  }
+
+  bool _selectedTheme = false;
+  bool get selectedTheme => _selectedTheme;
+  set setSelectedTheme(bool value) {
+    _selectedTheme = value;
+    if (_selectedTheme) {
+      setThemeMode = ThemeMode.dark;
+    }
+    if (!_selectedTheme) {
+      setThemeMode = ThemeMode.light;
+    }
+    notifyListeners();
+  }
+}
