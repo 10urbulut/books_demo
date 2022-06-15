7c92bd3d31678d6fddf2564c8257c5d50bfc8274
Onur Bulut <10urbulut@gmail.com>
2022-06-15 18:00:43 +0300

    Add project strings

---
+++ lib/common/constants/strings/title_strings.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: constant_identifier_names
+
+class TitleStrings {
+  static const String BOOKS = "Books";
+  static const String BOOK_DETAIL = "Book Detail";
+
+  static const String FAVORITE_BOOKS = "Favorite Book";
+}
