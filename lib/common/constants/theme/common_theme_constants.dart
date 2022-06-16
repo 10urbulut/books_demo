60c0c084dfb4d0a603e09ea61fd94a355c6e086c
Onur Bulut <10urbulut@gmail.com>
2022-06-16 14:57:09 +0300

    Add static theme constants

--- lib/common/constants/theme/common_theme_constants.dart
+++ lib/common/constants/theme/common_theme_constants.dart
@@ -1,3 +1,6 @@
 class CommonThemeConstants {
-  static const double appBarCircular = 55;
+  static double get appBarCircular => 55;
+  static double get generalCircular => 15;
+  static double get tablet => 800;
+  static double get bigScreen => 1700;
 }
