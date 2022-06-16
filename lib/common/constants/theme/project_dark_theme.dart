b756a609b68503ad3f204a3682ae5a62f3baf0df
Onur Bulut <10urbulut@gmail.com>
2022-06-16 14:57:54 +0300

    Add dark theme propertis

--- lib/common/constants/theme/project_dark_theme.dart
+++ lib/common/constants/theme/project_dark_theme.dart
@@ -1,7 +1,32 @@
+import 'package:books_demo/common/constants/theme/common_theme_constants.dart';
 import 'package:flutter/material.dart';
 
 class ProjectDarkTheme {
   static ThemeData get theme {
-    return ThemeData.dark().copyWith();
+    return ThemeData.dark().copyWith(
+      appBarTheme: _appbarTheme,
+      cardTheme: _cardTheme,
+    );
+  }
+
+  static AppBarTheme get _appbarTheme {
+    return AppBarTheme(
+      centerTitle: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(CommonThemeConstants.appBarCircular),
+          bottomRight: Radius.circular(CommonThemeConstants.appBarCircular),
+        ),
+      ),
+      elevation: 5,
+    );
+  }
+
+  static CardTheme get _cardTheme {
+    return CardTheme(
+      margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+      elevation: 5,
+    );
   }
 }
