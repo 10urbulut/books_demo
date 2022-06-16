f195ab13f64a5a7c6bce3e9cfe5a27ee73b336a1
Onur Bulut <10urbulut@gmail.com>
2022-06-16 14:59:34 +0300

    Edit toastr error string

--- lib/core/toastr/toastr_strings.dart
+++ lib/core/toastr/toastr_strings.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: constant_identifier_names
 
 class ToastrStrings {
-  static const String BIR_SORUN_OLUSTU = "Bir Sorun Oluştu";
+  static const String FAILED_TO_LOAD_BOOKS = 'Failed to load books';
 }
